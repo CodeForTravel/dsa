6ca8dee63623e4a6334c20e11b9a3cc2ece1c187
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-30 22:15:08 +0600

    wip

---
+++ dsa_data_type/stack_.py
@@ -0,0 +1,35 @@
+# Stack implementation in python
+
+
+# # Creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+# # Creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+
+# # Adding items into the stack
+def push(stack, item):
+    stack.append(item)
+    print(f"Pushed Item: {item}")
+
+
+# # Removing an element from the stack
+def pop(stack):
+    if check_empty(stack):
+        return "Stack is empty"
+    return stack.pop()
+
+
+stack = create_stack()
+push(stack, 1)
+push(stack, 2)
+push(stack, 3)
+push(stack, 4)
+push(stack, 5)
+print("popped item: " + str(pop(stack)))
+push(stack, 6)
+print("stack after popping an element: " + str(stack))
